9205d74ca5dc805307834a630cc11dbab3e55ba3
Victor Hugo Herrera Prieto <vherreraprieto@alumnoeseit.edu.co>
2024-08-23 02:34:54 -0500

    Fix responsive y funciones

--- app.js
+++ app.js
@@ -1,37 +1,47 @@
+const findCharacters = /[^a-z\s]/;
+
 function encriptar() {
-    let texto = document.getElementById("texto").value;
+  let texto = document.getElementById("texto").value;
+  console.log(findCharacters.test(texto));
+  if (findCharacters.test(texto)) {
+    alert("Solo letras minusculas y sin caracteres especiales");
+  } else {
     let textoEncriptado = texto
-    .replace(/a/g, "ai")
-    .replace(/e/g, "enter")
-    .replace(/i/g, "imes")
-    .replace(/o/g, "ober")
-    .replace(/u/g, "ufat");
+      .replace(/e/g, "enter")
+      .replace(/i/g, "imes")
+      .replace(/a/g, "ai")
+      .replace(/o/g, "ober")
+      .replace(/u/g, "ufat");
 
     document.getElementById("resultado").innerText = textoEncriptado;
   }
+}
 
-  function desencriptar() {
-    
-    let texto = document.getElementById("texto").value;
-    
+function desencriptar() {
+  let texto = document.getElementById("texto").value;
+  if (findCharacters.test(texto)) {
+    alert("Solo letras minusculas y sin caracteres especiales");
+  } else {
     let textoDesencriptado = texto
       .replace(/ai/g, "a")
       .replace(/enter/g, "e")
       .replace(/imes/g, "i")
       .replace(/ober/g, "o")
       .replace(/ufat/g, "u");
-    
+
     document.getElementById("resultado").innerText = textoDesencriptado;
   }
-  
-  function copiar() {
-    let resultado = document.getElementById("resultado").innerText;
-    navigator.clipboard
-      .writeText(resultado)
-      .then(() => {
-        alert("Texto copiado al portapapeles");
-      })
-      .catch((error) => {
-        console.error("Error al copiar el texto: ", error);
-      });
-  }
+}
+
+function copiar() {
+  let resultado = document.getElementById("resultado").value;
+  console.log(resultado);
+  navigator.clipboard
+    .writeText(resultado)
+    .then(() => {
+      alert("Texto copiado al portapapeles");
+    })
+    .catch((error) => {
+      console.error("Error al copiar el texto: ", error);
+    });
+}
